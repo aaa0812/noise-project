212c84b1deffe19cf3ab282896b7ce07bb678d6e
Elea Poirot <contact.elea.poirot@gmail.com>
2025-12-02 17:52:58 +0100

    ajout du formulaire de couleurs pour générer l'image avec l'api

--- script.js
+++ script.js
@@ -3,15 +3,34 @@
     document.addEventListener('DOMContentLoaded', init);
 
     async function init() {
-        const noiseImgEl = document.querySelector('.noise-img');
+        const noiseForm = document.querySelector('.noise-form'); //récupère le form dans le DOM
+        
+        noiseForm.addEventListener("submit", handleSubmit); //écoute la soumission du formulaire pour appeler la méthode handlesubmit   
+    }
+    
+    /**
+     * Fonction gérant la soumission du formulaire
+     * @param {*} e SubmitEvent
+    */
+   function handleSubmit(e) {
+       e.preventDefault();
+       let formData = new FormData(e.target);
+
+       datas = Object.fromEntries(formData);
+       getRGBImg(datas.red, datas.green, datas.blue);     
+    }
+    
+    async function getRGBImg(red, green, blue) {
+        const noiseImgEl = document.querySelector('.noise-img'); //récupère l'élément img dans le DOM
+
         try {
-            const res = await fetch(API_URL + "?r=250&g=48&b=242&mode=around&base64");
-            if(res.ok) {
-                const body = await res.json();
-                noiseImgEl.setAttribute("src", body.base64);
+            const res = await fetch(`${API_URL }?r=${red}&g=4${green}&b=${blue}&mode=around&base64`); //fait une requête à l'api
+            if (res.ok) { //si la réponse est bonne
+                const body = await res.json(); //converti la réponse en json pour pouvoir la lire
+                noiseImgEl.setAttribute("src", body.base64); //récupére la propriété base64 de l'objet pour le mettre dans le src de l'élément image
             }
-        } catch(er) {
-            console.log(er.message);
+        } catch (er) {
+            console.log(er.message);// affiche l'erreur dans la console
         }
     }
 }())
