bd2daabcf46a8ec0ada07154d76825f9bca8a3b5
gukeyboi <lla.kowalik.lain@gmail.com>
2025-12-15 22:41:38 +0100

    nouveau fetch avec + de variables

--- script.js
+++ script.js
@@ -18,10 +18,11 @@
 
        datas = Object.fromEntries(formData); //met les données dans un objet : { red : num, green: num, blue: num } pour y accéder facilement
        console.log(datas)
-       getRGBImg(datas.red, datas.green, datas.blue);     
+       //getRGBImg(datas.red, datas.green, datas.blue);
+       getAdvanced(datas.red, datas.green, datas.blue, datas.nbTiles, datas.tileSize, datas.borderWidth, datas.brightnessMode, datas.aroundMode, datas.brightnessSteps);     
     }
     
-    async function getRGBImg(red, green, blue, nbtiles=50) {
+    async function getRGBImg(red, green, blue) {
         const noiseImgEl = document.querySelector('.noise-img'); //récupère l'élément img dans le DOM
 
         try {
@@ -34,4 +35,19 @@
             console.log(er.message);// affiche l'erreur dans la console
         }
     }
+
+    async function getAdvanced(red, green, blue, nbTiles, tileSize, borderWidth, brightnessMode, aroundMode, brightnessSteps) {
+        
+        const noiseImgEl = document.querySelector('.noise-img'); //récupère l'élément img dans le DOM
+        try {
+            const res = await fetch(`${API_URL }?r=${red}&g=${green}&b=${blue}&tiles=${nbTiles}&tileSize=${tileSize}&borderWidth=${borderWidth}&base64`); //fait une requête à l'api
+            if (res.ok) { //si la réponse est bonne
+                const body = await res.json(); //converti la réponse en json pour pouvoir la lire
+                noiseImgEl.setAttribute("src", body.base64); //récupére la propriété base64 de l'objet pour le mettre dans le src de l'élément image
+            }
+        } catch (er) {
+            console.log(er.message);// affiche l'erreur dans la console
+        }
+    }
 }())
+
