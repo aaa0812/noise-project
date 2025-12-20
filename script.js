d304cd8569091d24c6caa106335526bde3351f23
gukeyboi <lla.kowalik.lain@gmail.com>
2025-12-20 13:54:13 +0100

    début random fetch

--- script.js
+++ script.js
@@ -4,16 +4,9 @@
 
     async function init() {
         const noiseForm = document.querySelector('.noise-form'); //récupère le form dans le DOM
-        const crtToggle = document.querySelector('.crt-check input');
-        const body = document.querySelector('body');
-
-        if (noiseForm) {
-            noiseForm.addEventListener("submit", handleSubmit); //écoute la soumission du formulaire pour appeler la méthode handlesubmit
-        }
-
-        if (crtToggle) {
-            crtToggle.addEventListener("click", () => { body.classList.toggle('effect-class') });
-        }
+        const random = document.querySelector('.random-btn');
+        noiseForm.addEventListener("submit", handleSubmit); //écoute la soumission du formulaire pour appeler la méthode handlesubmit
+        random.addEventListener("click", handleRandom);
         initBurgerMenu();
     }
 
@@ -24,6 +17,22 @@
         burger.addEventListener('click', () => {
             menu.classList.toggle('active');
         });
+    }
+
+    async function handleRandom() {
+
+        const noiseImgEl = document.querySelector('.noise-img'); //récupère l'élément img dans le DOM
+        console.log("random")
+        try {
+            let res;
+            res = await fetch(`${API_URL}?base64`); //fait une requête à l'api
+            if (res.ok) { //si la réponse est bonne
+                const body = await res.json(); //converti la réponse en json pour pouvoir la lire
+                noiseImgEl.setAttribute("src", body.base64); //récupére la propriété base64 de l'objet pour le mettre dans le src de l'élément image
+            }
+        } catch (er) {
+            console.error(er.message);// affiche l'erreur dans la console
+        }
     }
 
     /**
@@ -37,12 +46,14 @@
         datas = Object.fromEntries(formData); //met les données dans un objet : { red : num, green: num, blue: num } pour y accéder facilement
         getAdvanced(datas.red, datas.green, datas.blue, datas.hex, datas.nbTiles, datas.tileSize, datas.borderWidth, datas.mode, datas.brightnessSteps, datas.brightnessMultiplicator);
     }
+    
 
     async function getAdvanced(red, green, blue, hex, nbTiles = 50, tileSize = 7, borderWidth = 0, mode = "around", brightnessSteps = 5, brightnessMultiplicator = 1.5) {
 
         const noiseImgEl = document.querySelector('.noise-img'); //récupère l'élément img dans le DOM
         try {
             let res;
+            
             if (hex === "") {
                 res = await fetch(`${API_URL}?r=${red}&g=${green}&b=${blue}&tiles=${nbTiles}&tileSize=${tileSize}&borderWidth=${borderWidth}&mode=${mode}&steps=${brightnessSteps}&multi=${brightnessMultiplicator}&base64`); //fait une requête à l'api
             } else {
